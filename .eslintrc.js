380220ce82d9498691d8a9e937a339421c7f061b
宋 迪 <670165552@qq.com>
2021-05-26 17:00:58 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,38 +29,59 @@ module.exports = {
 
     // https://github.com/prettier/eslint-config-prettier#installation
     // usage with Prettier, provided by 'eslint-config-prettier'.
-    'prettier'
+    'standard'
   ],
 
   plugins: [
     // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-file
     // required to lint *.vue files
     'vue',
-
+    'quasar'
     // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
     // Prettier has not been included as plugin to avoid performance impact
     // add it as an extension for your IDE
   ],
 
   globals: {
-    ga: 'readonly', // Google Analytics
-    cordova: 'readonly',
-    __statics: 'readonly',
-    __QUASAR_SSR__: 'readonly',
-    __QUASAR_SSR_SERVER__: 'readonly',
-    __QUASAR_SSR_CLIENT__: 'readonly',
-    __QUASAR_SSR_PWA__: 'readonly',
-    process: 'readonly',
-    Capacitor: 'readonly',
-    chrome: 'readonly'
+    ga: true, // Google Analytics
+    cordova: true,
+    __statics: true,
+    __QUASAR_SSR__: true,
+    __QUASAR_SSR_SERVER__: true,
+    __QUASAR_SSR_CLIENT__: true,
+    __QUASAR_SSR_PWA__: true,
+    Prism: true
   },
 
   // add your custom rules here
   rules: {
+    'brace-style': [ 2, 'stroustrup', { 'allowSingleLine': true } ],
+    'prefer-const': 2,
     'prefer-promise-reject-errors': 'off',
+    'multiline-ternary': 'off',
+    'array-bracket-spacing': [ 'error', 'always', { singleValue: false } ],
+    'computed-property-spacing': [ 'error', 'always' ],
+    'no-prototype-builtins': 'off',
+    'no-case-declarations': 'off',
+    'generator-star-spacing': 'off',
+    'arrow-parens': 'off',
+    'one-var': 'off',
+    'no-void': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
 
+    'import/export': 'off',
+    'import/first': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-duplicates': 'off',
+    'import/no-named-default': 'off',
+    'import/no-webpack-loader-syntax': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
 
-    // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'quasar/check-valid-props': 'warn',
+
+    'vue/no-mutating-props': 'off',
+    'vue/no-v-model-argument': 'off'
   }
 }
